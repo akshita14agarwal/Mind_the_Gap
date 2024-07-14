440a6b754518020a93a03281d6c6962889270759
Akshita Agarwal <akshita.20225006@mnnit.ac.in>
2024-07-14 14:03:25 +0530

    jwt authentication

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,5 +1,8 @@
 import User from "../models/User.js"
 import bcrypt from "bcryptjs"
+import {createError} from "../utils/error.js";
+import jwt from "jsonwebtoken";
+
 export const register = async(req, res, next)=>{
     try{
     const salt = bcrypt.genSaltSync(10);
@@ -9,7 +12,7 @@ export const register = async(req, res, next)=>{
         username: req.body.username,
         email: req.body.email,
         password: hash,
-      })
+      });
 
       await newUser.save()
       res.status(200).send("User has been created.")
@@ -18,3 +21,27 @@ export const register = async(req, res, next)=>{
     }
 
 };
+export const login = async(req, res, next)=>{
+  try{
+    const user =await  User.findOne({username: req.body.username})
+    if(!user) return next(createError(404, "User not found"))
+
+      const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
+      if(!isPasswordCorrect) return next(createError(400,"Wrong Password!"));
+
+      const token = jwt.sign({id: user._id, isAdmin: user.isAdmin},process.env.JWT );
+
+
+        const{password, isAdmin, ...otherDetails} = user._doc; 
+        res
+        .cookie("access_token", token,{
+          httpOnly:true,
+        })
+        .status(200)
+        .json({...otherDetails});
+  }catch(err){
+      next(err);
+  }
+
+};
+
